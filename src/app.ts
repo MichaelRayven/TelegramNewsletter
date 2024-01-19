07bdd4fea4671eb0d33e7ad637eba7d7c29f3143
MichaelRayven <themikerayven@gmail.com>
2024-01-19 07:43:26 +0700

    Changed routes, added tsc-alias depencency

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import express from "express"
 import 'dotenv/config'
 
+import cors from "cors"
 import passport from "@/auth/auth"
 import authRouter from "@/routers/authRouter"
 import secureRouter from "@/routers/secureRouter"
+import { apiVersion } from "./config/config"
 
 const app = express()
 
 // Configure express
 app.use(express.json())
 app.use(express.urlencoded());
+app.use(cors())
 
 // Initiate passport
 app.use(passport.initialize())
 
 // Routes
-app.use('/', authRouter)
-app.use('/user', passport.authenticate('jwt', { session: false }), secureRouter);
+app.use(`/${apiVersion}/user`, authRouter)
+app.use(`/${apiVersion}/user`, passport.authenticate('jwt', { session: false }), secureRouter);
 
 app.listen(process.env.PORT, () => {
  console.log("Listening on port: " + process.env.PORT);
